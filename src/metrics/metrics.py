a0b3972ce91faffbf97e95006c5666a2693a4c71
RandyHuynh5815 <randyhuynh5815@gmail.com>
2024-06-14 10:40:43 -0700

    Updated LDJ to take in waypoint data

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -1,10 +1,10 @@
 import jax.numpy as jnp
 
-def log_dimensionless_jerk(waypoints: jnp.ndarray) -> float:
+def total_dist_travelled(waypoints: jnp.ndarray) -> float:
     return jnp.sum(jnp.linalg.norm(jnp.diff(waypoints, axis=0), axis=1))
 
-def total_dist_travelled(velocities: jnp.ndarray, duration: float) -> float:
-    max_velocity = jnp.max(jnp.linalg.norm(velocities, axis=1))
+def log_dimensionless_jerk(waypoints: jnp.ndarray, duration: float) -> float:
+    max_velocity = jnp.max(jnp.linalg.norm(jnp.diff(waypoints, axis=0)/duration, axis=1))
     scale = jnp.pow(duration, 3)/jnp.pow(max_velocity, 2)
-    jerk = jnp.pow(jnp.linalg.norm(jnp.diff(velocities, n=2, axis=0)/jnp.pow(1/duration,2)), 2)
+    jerk = jnp.pow(jnp.linalg.norm(jnp.diff(waypoints, n=3, axis=0)/jnp.pow(1/duration,3)), 2)
     return -jnp.log((scale*jerk)/duration)
